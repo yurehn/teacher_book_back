bbc4c7043b389def4affb8af51518b2412468fa3
yurehn <d.carrascoquinchn@uandresbello.edu>
2023-03-20 17:42:26 -0300

    se creo controller, DTO, routes y Validators de tabla subject_teacher

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,7 @@ import courseRoutes from './courseRoutes'
 import studentRoutes from './studentRoutes'
 import annotationRoutes from "./annotationRoutes"
 import teacherRoutes from './teacherRoutes'
+import subject_teacher from './subject_teacherRoutes'
 
 
 const apiRoutes = Router()
@@ -11,5 +12,6 @@ apiRoutes.use('/courses', courseRoutes)
 apiRoutes.use('/student', studentRoutes)
 apiRoutes.use('/annotation', annotationRoutes)
 apiRoutes.use('/teacher', teacherRoutes)
+apiRoutes.use('/subject_teacher', subject_teacher)
 
 export default apiRoutes
