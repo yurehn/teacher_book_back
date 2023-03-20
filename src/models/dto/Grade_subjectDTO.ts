3b822b4ba47c42f089cf053cdb1806d8194e3b1f
yurehn <d.carrascoquinchn@uandresbello.edu>
2023-03-20 18:29:35 -0300

    se creo controller, DTO, validators y routes de grade_subject

---
+++ src/models/dto/Grade_subjectDTO.ts
@@ -0,0 +1,13 @@
+interface BaseGrade_subjectDTO {
+  id?: number
+  gradeId: number
+  subjectId: number
+}
+
+export interface Grade_subjectDTO extends BaseGrade_subjectDTO {
+  id: number
+}
+
+export interface CreateGrade_subjectDTO extends BaseGrade_subjectDTO { }
+
+export interface UpdateGrade_subjectDTO extends Partial<BaseGrade_subjectDTO> { }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,7 @@ import studentRoutes from './studentRoutes'
 import annotationRoutes from "./annotationRoutes"
 import teacherRoutes from './teacherRoutes'
 import subject_teacher from './subject_teacherRoutes'
+import grade_subject from './grade_subjectRoutes'
 
 
 const apiRoutes = Router()
@@ -13,5 +14,6 @@ apiRoutes.use('/student', studentRoutes)
 apiRoutes.use('/annotation', annotationRoutes)
 apiRoutes.use('/teacher', teacherRoutes)
 apiRoutes.use('/subject_teacher', subject_teacher)
+apiRoutes.use('/grade_subject', grade_subject)
 
 export default apiRoutes
