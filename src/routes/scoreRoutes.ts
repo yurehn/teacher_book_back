2a589eabbef9a10d790161931883e124c14823da
yurehn <d.carrascoquinchn@uandresbello.edu>
2023-05-16 17:12:53 -0400

    refactor[routes]: refact routes for new design

---
+++ src/routes/scoreRoutes.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express'
+import ScoreController from '../controllers/ScoreController'
+
+
+const controller = new ScoreController()
+const scoreRoutes = Router()
+
+scoreRoutes.get('/', controller.getAll)
+// scoreRoutes.get('/:id', controller.getById)
+// scoreRoutes.post('/', controller.create)
+// scoreRoutes.put('/:id', controller.update)
+// scoreRoutes.delete('/:id', controller.delete)
+
+export default scoreRoutes
--- src/routes/studentRoutes.ts
+++ src/routes/studentRoutes.ts
@@ -6,9 +6,9 @@ const studentRoutes = Router()
 const controller = new StudentController()
 
 studentRoutes.get('/', controller.getAll)
-studentRoutes.get('/:id', controller.getById)
-studentRoutes.post('/', controller.create)
-studentRoutes.put('/:id', controller.update)
-studentRoutes.delete('/:id', controller.delete)
+// studentRoutes.get('/:id', controller.getById)
+// studentRoutes.post('/', controller.create)
+// studentRoutes.put('/:id', controller.update)
+// studentRoutes.delete('/:id', controller.delete)
 
 export default studentRoutes
